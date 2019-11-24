54785c720895d0f0ffecac41ebbea7899c252f00
Lukasz Maciejewski <lukasz.m.maciejewski@gmail.com>
2019-11-24 20:57:41 +0100

    Porting things from radian and svarog
    - defadvice and defhook wrappers from Radian
    - editing tweaks from svarog/graphene
    - projectile config
    - c and c++ config

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
 
-;;; Code:
+;;;; Code:
 (setq gc-cons-threshold 100000000)
 (setq make-backup-files nil) ; stop creating backup~ files
 (setq auto-save-default nil) ; stop creating #autosave# files
@@ -37,10 +37,71 @@
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
 
+(defmacro svarog/defadvice (name arglist where place docstring &rest body)
+  "Define an advice called NAME and add it to a function.
+ARGLIST is as in `defun'. WHERE is a keyword as passed to
+`advice-add', and PLACE is the function to which to add the
+advice, like in `advice-add'. DOCSTRING and BODY are as in
+`defun'."
+  (declare (indent 2)
+           (doc-string 5))
+  (unless (stringp docstring)
+    (error "Svarog: no docstring provided for `svarog/defadvice'"))
+  `(progn
+     (eval-and-compile
+       (defun ,name ,arglist
+         ,(let ((article (if (string-match-p "^:[aeiou]" (symbol-name where))
+                             "an"
+                           "a")))
+            (format "%s\n\nThis is %s `%S' advice for `%S'."
+                    docstring article where
+                    (if (and (listp place)
+                             (memq (car place) ''function))
+                        (cadr place)
+                      place)))
+         ,@body))
+     (advice-add ',place ',where #',name)
+     ',name))
+
+(defmacro svarog/defhook (name arglist hooks docstring &rest body)
+  "Define a function called NAME and add it to a hook.
+ARGLIST is as in `defun'. HOOKS is a list of hooks to which to
+add the function, or just a single hook. DOCSTRING and BODY are
+as in `defun'."
+  (declare (indent 2)
+           (doc-string 4))
+  (unless (listp hooks)
+    (setq hooks (list hooks)))
+  (dolist (hook hooks)
+    (unless (string-match-p "-\\(hook\\|functions\\)$" (symbol-name hook))
+      (error "Symbol `%S' is not a hook" hook)))
+  (unless (stringp docstring)
+    (error "Svarog: no docstring provided for `svarog/defhook'"))
+  (let ((hooks-str (format "`%S'" (car hooks))))
+    (dolist (hook (cdr hooks))
+      (setq hooks-str (format "%s\nand `%S'" hooks-str hook)))
+    `(progn
+       (defun ,name ,arglist
+         ,(format "%s\n\nThis function is for use in %s."
+                  docstring hooks-str)
+         ,@body)
+       (dolist (hook ',hooks)
+         (add-hook hook ',name)))))
+
+;;; Prevent Emacs-provided Org from being loaded
+
+;; Our real configuration for Org comes much later. Doing this now
+;; means that if any packages that are installed in the meantime
+;; depend on Org, they will not accidentally cause the Emacs-provided
+;; (outdated and duplicated) version of Org to be loaded before the
+;; real one is registered.
+(straight-use-package 'org)
 
 (straight-use-package 'use-package)
 
 (straight-use-package 'git)
+
+(setq vc-handled-backends nil) ; disables build-in version control
 (straight-use-package 'magit)
 
 (defconst svarog/config-local-directory
@@ -55,7 +116,6 @@
 (setq recentf-save-file (expand-file-name "recentf" svarog/config-local-directory))
 
 (use-package solarized-theme
-  :straight t
   :config
   (setq solarized-distinct-fringe-background t
         solarized-use-variable-pitch nil
@@ -69,3 +129,110 @@
 
 (straight-use-package 'rust-mode)
 
+(straight-use-package 'cquery
+  :config
+  (add-hook 'c++-mode-hook (lambda ()
+                             (lsp-cquery-enable))))
+
+(with-eval-after-load 'projectile
+  (projectile-register-project-type 'cpp-code '("projectile-cpp")
+                                    :configure "(mkdir build-debug; cd build-debug; cmake ../source -GNinja -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug; cd ..; ln -s build-debug/compile_commands.json)"
+                                    :src-dir "source"
+                                    :compile "(cd build-debug; cmake --build . )"
+                                      :test "(cd build-debug; ctest .)"
+                                      :test-suffix ".test.cpp"
+                                      ))
+
+(use-package projectile
+  :config
+  (setq projectile-cache-file (concat svarog/config-local-directory "projectile.cache")
+        projectile-known-projects-file (expand-file-name "projectile-bookmarks.eld"
+                                                         svarog/config-local-directory))
+  (projectile-mode +1))
+
+;;; C++ config:
+(svarog/defhook c-mode-common-configuration
+		"Common conf for C mode."
+		(c-set-style "bsd")
+		(setq c-basic-offset 4
+		      tab-width 4
+		      indent-tabs-mode nil
+		      c-tab-always-indent t
+		      c-echo-syntactic-information-p t)
+		(define-key c-mode-base-map (kbd "RET") 'newline-and-indent)
+
+		(auto-revert-mode 1)
+		(toggle-truncate-lines t))
+		
+(svarog/defhook c++-mode-configuration () c++-mode-hook
+		"Custom C++ indent config."
+		(add-to-list 'c-offsets-alist '(innamespace . 0))
+		(c-set-offset 'substatement-open 0)
+		(c-set-offset 'label '+))
+
+(straight-use-package 'diminish)
+(diminish 'company-mode)
+(diminish 'visual-line-mode)
+(diminish 'which-key-mode)
+(diminish 'auto-revert-mode)
+(diminish 'smartparens-mode)
+(diminish 'eldoc-mode)
+(diminish 'lsp-mode)
+(diminish 'whitespace-mode)
+(diminish 'hs-minor-mode)
+(diminish 'ivy-mode)
+(diminish 'projectile-mode "P")
+(diminish 'helm-mode)
+
+(display-battery-mode t)
+
+(straight-use-package 'ledger-mode)
+
+(straight-use-package 'helm)
+(global-set-key (kbd "M-x") #'helm-M-x)
+(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
+(global-set-key (kbd "C-x C-f") #'helm-find-files)
+(helm-mode +1)
+
+(straight-use-package 'bazel-mode)
+
+;;;; Editing tweaks
+
+(use-package 'undo-tree
+  :config
+  (diminish 'undo-tree-mode))
+(global-undo-tree-mode)
+
+(electic-indent-mode t)
+(setq linum-format " %4d ")
+(linum-mode t)
+
+(straight-use-package 'smartparens
+		      )
+(smartparens-mode t))
+(smartparens-global-mode t)
+
+;; Delete marked text on typing
+(delete-selection-mode t)
+
+;; Soft-wrap lines
+(global-visual-line-mode t)
+
+;; Better scrolling with mouse wheel/trackpad.
+(unless (and (boundp 'mac-mouse-wheel-smooth-scroll) mac-mouse-wheel-smooth-scroll)
+  (global-set-key [wheel-down] (lambda () (interactive) (scroll-up-command 1)))
+  (global-set-key [wheel-up] (lambda () (interactive) (scroll-down-command 1)))
+  (global-set-key [double-wheel-down] (lambda () (interactive) (scroll-up-command 2)))
+  (global-set-key [double-wheel-up] (lambda () (interactive) (scroll-down-command 2)))
+  (global-set-key [triple-wheel-down] (lambda () (interactive) (scroll-up-command 4)))
+  (global-set-key [triple-wheel-up] (lambda () (interactive) (scroll-down-command 4))))
+
+;; Character encodings default to utf-8.
+(prefer-coding-system 'utf-8)
+(set-language-environment 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+
+;; apply syntax highlighting to all buffers
+(global-font-lock-mode t)
