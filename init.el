359a9c5aa35e9aff15c878fd8fc64d2a1ce8da61
Lukasz Maciejewski <lukasz.m.maciejewski@gmail.com>
2019-11-24 11:56:19 +0100

    Adding straight.el bootstrap and basic setup

---
+++ init.el
@@ -0,0 +1,71 @@
+
+;;; Code:
+(setq gc-cons-threshold 100000000)
+(setq make-backup-files nil) ; stop creating backup~ files
+(setq auto-save-default nil) ; stop creating #autosave# files
+
+;; Save backup files in the temporary directory
+(setq backup-directory-alist `((".*" . ,temporary-file-directory))
+      auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
+
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(delete-selection-mode 1)
+;; Don't show the startup message
+(setq inhibit-startup-message t)
+
+(setq straight-repository-branch "develop")
+(setq straight-use-package-by-default t)
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;;;;  package.el
+;;; so package-list-packages includes them
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+
+
+(straight-use-package 'use-package)
+
+(straight-use-package 'git)
+(straight-use-package 'magit)
+
+(defconst svarog/config-local-directory
+  (concat user-emacs-directory "config-local/"))
+(unless (file-exists-p svarog/config-local-directory)
+  (make-directory svarog/config-local-directory))
+
+(defconst svarog/custom-file (expand-file-name "custom.el" svarog/config-local-directory))
+(setq custom-file svarog/custom-file)
+(load svarog/custom-file 'noerror)
+
+(setq recentf-save-file (expand-file-name "recentf" svarog/config-local-directory))
+
+(use-package solarized-theme
+  :straight t
+  :config
+  (setq solarized-distinct-fringe-background t
+        solarized-use-variable-pitch nil
+        solarized-high-contrast-mode-line t
+        solarized-height-minus-1 1.0
+        solarized-height-plus-1 1.0
+        solarized-height-plus-2 1.0
+        solarized-height-plus-3 1.0
+        solarized-height-plus-4 1.0))
+(load-theme 'solarized-dark t)
+
+(straight-use-package 'rust-mode)
+
