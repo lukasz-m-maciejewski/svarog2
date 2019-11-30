e396bc723222ddd3b7b2c5b8a6d667fb2cf0d70e
Lukasz Maciejewski <lukasz.m.maciejewski@pm.me>
2019-11-30 10:28:09 +0100

    Adding company configuration

--- init.el
+++ init.el
@@ -191,10 +191,149 @@ as in `defun'."
   :config
   (add-hook 'lsp-mode-hook 'lsp-ui-mode))
 
+;;;; Autocompletion
+
+;; Package `company' provides an in-buffer autocompletion framework.
+;; It allows for packages to define backends that supply completion
+;; candidates, as well as optional documentation and source code. Then
+;; Company allows for multiple frontends to display the candidates,
+;; such as a tooltip menu. Company stands for "Complete Anything".
+(use-package company
+  :defer 0.5
+  :init
+
+  (defvar radian--company-backends-global
+    '(company-capf
+      company-files
+      (company-dabbrev-code company-keywords)
+      company-dabbrev)
+    "Values for `company-backends' used everywhere.
+If `company-backends' is overridden by Radian, then these
+backends will still be included.")
+
+  :bind (;; Remap the standard Emacs keybindings for invoking
+         ;; completion to instead use Company. You might think this
+         ;; could be put in the `:bind*' declaration below, but it
+         ;; seems that `bind-key*' does not work with remappings.
+         ([remap completion-at-point] . company-manual-begin)
+         ([remap complete-symbol] . company-manual-begin)
+
+         ;; The following are keybindings that take effect whenever
+         ;; the completions menu is visible, even if the user has not
+         ;; explicitly interacted with Company.
+
+         :map company-active-map
+
+         ;; Make TAB always complete the current selection, instead of
+         ;; only completing a common prefix.
+         ("<tab>" . company-complete-selection)
+         ("TAB" . company-complete-selection)
+
+         ;; The following are keybindings that only take effect if the
+         ;; user has explicitly interacted with Company. Note that
+         ;; `:map' from above is "sticky", and applies also below: see
+         ;; https://github.com/jwiegley/use-package/issues/334#issuecomment-349473819.
+
+         :filter (company-explicit-action-p)
+
+         ;; Make RET trigger a completion if and only if the user has
+         ;; explicitly interacted with Company, instead of always
+         ;; doing so.
+         ("<return>" . company-complete-selection)
+         ("RET" . company-complete-selection)
+
+         ;; We then make <up> and <down> abort the completions menu
+         ;; unless the user has interacted explicitly. Note that we
+         ;; use `company-select-previous' instead of
+         ;; `company-select-previous-or-abort'. I think the former
+         ;; makes more sense since the general idea of this `company'
+         ;; configuration is to decide whether or not to steal
+         ;; keypresses based on whether the user has explicitly
+         ;; interacted with `company', not based on the number of
+         ;; candidates.
+         ;;
+         ;; Note that M-p and M-n work regardless of whether explicit
+         ;; interaction has happened yet, and note also that M-TAB
+         ;; when the completions menu is open counts as an
+         ;; interaction.
+         ("<up>" . company-select-previous)
+         ("<down>" . company-select-next))
+
+  :bind* (;; The default keybinding for `completion-at-point' and
+          ;; `complete-symbol' is M-TAB or equivalently C-M-i. We
+          ;; already remapped those bindings to `company-manual-begin'
+          ;; above. Here we make sure that they definitely invoke
+          ;; `company-manual-begin' even if a minor mode binds M-TAB
+          ;; directly.
+          ("M-TAB" . company-manual-begin))
+
+  :config
+
+  ;; Make completions display twice as soon.
+  (setq company-idle-delay 0.15)
+
+  ;; Make completions display when you have only typed one character,
+  ;; instead of three.
+  (setq company-minimum-prefix-length 1)
+
+  ;; Always display the entire suggestion list onscreen, placing it
+  ;; above the cursor if necessary.
+  (setq company-tooltip-minimum company-tooltip-limit)
+
+  ;; Always display suggestions in the tooltip, even if there is only
+  ;; one. Also, don't display metadata in the echo area. (This
+  ;; conflicts with ElDoc.)
+  (setq company-frontends '(company-pseudo-tooltip-frontend))
+
+  ;; Show quick-reference numbers in the tooltip. (Select a completion
+  ;; with M-1 through M-0.)
+  (setq company-show-numbers t)
+
+  ;; Prevent non-matching input (which will dismiss the completions
+  ;; menu), but only if the user interacts explicitly with Company.
+  (setq company-require-match #'company-explicit-action-p)
+
+  ;; Company appears to override our settings in `company-active-map'
+  ;; based on `company-auto-complete-chars'. Turning it off ensures we
+  ;; have full control.
+  (setq company-auto-complete-chars nil)
+
+  ;; Only search the current buffer to get suggestions for
+  ;; `company-dabbrev' (a backend that creates suggestions from text
+  ;; found in your buffers). This prevents Company from causing lag
+  ;; once you have a lot of buffers open.
+  (setq company-dabbrev-other-buffers nil)
+
+  ;; Make the `company-dabbrev' backend fully case-sensitive, to
+  ;; improve the UX when working with domain-specific words that have
+  ;; particular casing.
+  (setq company-dabbrev-ignore-case nil)
+  (setq company-dabbrev-downcase nil)
+
+  ;; When candidates in the autocompletion tooltip have additional
+  ;; metadata, like a type signature, align that information to the
+  ;; right-hand side. This usually makes it look neater.
+  (setq company-tooltip-align-annotations t)
+
+  (global-company-mode +1)
+
+  :blackout t)
+
+;; Package `company-prescient' provides intelligent sorting and
+;; filtering for candidates in Company completions.
+(use-package company-prescient
+  :demand t
+  :after company
+  :config
+
+  ;; Use `prescient' for Company menus.
+  (company-prescient-mode +1))
+
 (use-package company-lsp
   :demand t
   :config
   (push 'company-lsp company-backends))
+
 
 ;;; C++ config:
 (svarog/defhook c-mode-common-configuration () c-mode-common-hook
@@ -286,9 +425,7 @@ as in `defun'."
   (svarog/defhook autoformat-on-save () bazel-mode-hook "Autoformat on save."
                   (add-hook 'before-save-hook #'bazel-format nil t)))
 
-(add-hook 'bazel-mode-hook
-              (lambda ()
-                (add-hook 'before-save-hook #'bazel-format nil t)))
+(use-package protobuf-mode)
 
 ;;;; Editing tweaks
 
@@ -306,6 +443,7 @@ as in `defun'."
                 (setq whitespace-line-column 80)
                 (setq whitespace-style '(face tabs empty trailing newline))
                 (whitespace-mode t)
+                (blackout 'whitespace-mode)
                 (hl-line-mode t)
                 (hi-lock-mode t)
                 (hs-minor-mode t)
@@ -342,6 +480,7 @@ as in `defun'."
 (global-visual-line-mode t)
 
 (setq-default indent-tabs-mode nil)
+(setq-default python-intent-offset 4)
 
 ;; Better scrolling with mouse wheel/trackpad.
 (unless (and (boundp 'mac-mouse-wheel-smooth-scroll) mac-mouse-wheel-smooth-scroll)
