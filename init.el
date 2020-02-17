eea9d523c5662d2384b2e6c5d5f2661f6047272e
Lukasz Maciejewski <lukasz.m.maciejewski@pm.me>
2020-02-17 22:02:13 +0100

    adding racer package

--- init.el
+++ init.el
@@ -706,6 +706,16 @@ as in `defun'."
   :bind ("C-c f" . clang-format-buffer))
 
 (use-package rust-mode)
+(use-package racer
+  :demand t
+  :blackout t
+  :config
+  (add-hook 'rust-mode-hook #'racer-mode)
+  (add-hook 'racer-mode-hook #'eldoc-mode)
+  (add-hook 'racer-mode-hook #'company-mode)
+  (require 'rust-mode)
+  (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+  (setq company-tooltip-align-annotations t))
 
 (display-battery-mode t)
 
